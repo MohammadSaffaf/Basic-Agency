4e9b856ce94e690448f5cf8b34cf13e36434e08a
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-12-26 19:10:38 +0100

    About Gallery

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,6 +8,19 @@ function normalImg(index) {
     index.style.height = "50vh";
 
 }
+
+
+function Front(team) {
+    console.log("object");
+    team.style.zIndex = 0;
+    
+
+}
+
+function normalZindex(team) {
+    team.style.zIndex = 3;
+
+}
 const burger = document.getElementById("hamburger")
 const closeBurger = document.getElementById("close")
 const hamburger = document.getElementById("overlay")
